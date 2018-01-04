b7501b366440c754d0011d5bef92149e825ac1a3
Alex Coelho <alexcvcoelho@gmail.com>
2018-01-04 20:02:56 -0200

    assets pipeline para cada controller

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,13 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+ Rails.application.config.assets.precompile += %w( addresses.js
+                                                    contacts.js
+                                                    home.js
+                                                    kinds.js
+                                                    phones.js
+                                                    addresses.scss
+                                                    contacts.scss
+                                                    home.scss
+                                                    kinds.scss
+                                                    phones.scss)
